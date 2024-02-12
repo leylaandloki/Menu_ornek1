e1be17baad0c459031b86141125f1cd32d78d8c9
leylaandloki <145972621+leylaandloki@users.noreply.github.com>
2024-02-12 12:11:57 +0300

    Add files via upload

---
+++ MenuStrip_orenek _304/MenuStrip_orenek _304/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenuStrip_orenek__304
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ekleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OgrenciEkle ogrenciEkle = new OgrenciEkle();
+            ogrenciEkle.ShowDialog();
+        }
+
+        private void kapatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void ogrenciEkleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OgrenciEkle ogrenciEkle = new OgrenciEkle();
+            ogrenciEkle.ShowDialog();
+        }
+
+        private void kalınToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionFont = new Font(richTextBox1.Font, FontStyle.Bold);
+        }
+
+        private void ptToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font("Eras Bold ITC",12);
+        }
+
+        private void ptToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font("Goudy Stout", 15);
+        }
+
+        private void ptToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font("Sitka Subheading", 20);
+        }
+
+        private void formuKapatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
